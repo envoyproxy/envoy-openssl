f7f3ff31a32c1f6fafb8a363ef4a3bf123b6a2f2
Venil Noronha <veniln@vmware.com>
2019-11-01 11:52:37 -0700

    Update APIs
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- README.md
+++ README.md
@@ -8,10 +8,6 @@ The Envoy OpenSSL Extensions project hosts extensions for building
 [![Azure Pipeline](https://img.shields.io/azure-devops/build/cncf/d1341aaf-5711-4800-816d-4295da428269/12)](https://dev.azure.com/cncf/envoy-openssl/_build?definitionId=12)
 [![License](https://img.shields.io/badge/license-Apache--2.0-blue.svg)](LICENSE)
 
-Note that the following extensions are disabled at the moment.
-
-* `envoy.grpc_credentials.aws_iam`
-
 ## Building
 
 To build OpenSSL-enabled Envoy run the following command.
--- source/extensions/transport_sockets/tls/ssl_socket.cc
+++ source/extensions/transport_sockets/tls/ssl_socket.cc
@@ -308,7 +308,7 @@ bool SslSocketInfo::peerCertificatePresented() const {
   return cert != nullptr;
 }
 
-std::vector<std::string> SslSocketInfo::uriSanLocalCertificate() const {
+absl::Span<const std::string> SslSocketInfo::uriSanLocalCertificate() const {
   if (!cached_uri_san_local_certificate_.empty()) {
     return cached_uri_san_local_certificate_;
   }
@@ -323,7 +323,7 @@ std::vector<std::string> SslSocketInfo::uriSanLocalCertificate() const {
   return cached_uri_san_local_certificate_;
 }
 
-std::vector<std::string> SslSocketInfo::dnsSansLocalCertificate() const {
+absl::Span<const std::string> SslSocketInfo::dnsSansLocalCertificate() const {
   if (!cached_dns_san_local_certificate_.empty()) {
     return cached_dns_san_local_certificate_;
   }
@@ -408,7 +408,7 @@ const std::string& SslSocketInfo::urlEncodedPemEncodedPeerCertificateChain() con
   return cached_url_encoded_pem_encoded_peer_cert_chain_;
 }
 
-std::vector<std::string> SslSocketInfo::uriSanPeerCertificate() const {
+absl::Span<const std::string> SslSocketInfo::uriSanPeerCertificate() const {
   if (!cached_uri_san_peer_certificate_.empty()) {
     return cached_uri_san_peer_certificate_;
   }
@@ -422,7 +422,7 @@ std::vector<std::string> SslSocketInfo::uriSanPeerCertificate() const {
   return cached_uri_san_peer_certificate_;
 }
 
-std::vector<std::string> SslSocketInfo::dnsSansPeerCertificate() const {
+absl::Span<const std::string> SslSocketInfo::dnsSansPeerCertificate() const {
   if (!cached_dns_san_peer_certificate_.empty()) {
     return cached_dns_san_peer_certificate_;
   }
