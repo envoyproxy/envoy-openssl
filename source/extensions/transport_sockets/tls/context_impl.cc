bb38929426003b6ae6fded85d152dcf103b0555b
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2019-12-17 11:32:54 +0200

    get rid of old-style casts in assertions
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>

--- source/extensions/transport_sockets/tls/context_impl.cc
+++ source/extensions/transport_sockets/tls/context_impl.cc
@@ -1046,7 +1046,7 @@ int ServerContextImpl::sessionTicketProcess(SSL*, uint8_t* key_name, uint8_t* iv
 
     // This RELEASE_ASSERT is logically a static_assert, but we can't actually get
     // EVP_CIPHER_key_length(cipher) at compile-time
-    RELEASE_ASSERT(key.aes_key_.size() == (unsigned) EVP_CIPHER_key_length(cipher), "");
+    RELEASE_ASSERT(key.aes_key_.size() == static_cast<unsigned>(EVP_CIPHER_key_length(cipher)), "");
     if (!EVP_EncryptInit_ex(ctx, cipher, nullptr, key.aes_key_.data(), iv)) {
       return -1;
     }
@@ -1067,7 +1067,7 @@ int ServerContextImpl::sessionTicketProcess(SSL*, uint8_t* key_name, uint8_t* iv
           return -1;
         }
 
-        RELEASE_ASSERT(key.aes_key_.size() == (unsigned) EVP_CIPHER_key_length(cipher), "");
+        RELEASE_ASSERT(key.aes_key_.size() == static_cast<unsigned>(EVP_CIPHER_key_length(cipher)), "");
         if (!EVP_DecryptInit_ex(ctx, cipher, nullptr, key.aes_key_.data(), iv)) {
           return -1;
         }
