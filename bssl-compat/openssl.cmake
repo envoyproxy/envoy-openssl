2a693e66ce648e8e7b05f6bd24a7628d49cd42ef
Patrik Flykt <patrik.flykt@intel.com>
2022-10-17 14:00:38 +0300

    build: Detect already installed OpenSSL libraries
    When building the Envoy handshake extension, it is not possible to
    download OpenSSL anymore from the Envoy build environment. Thus
    update CMake build files to detect if the build environment already
    has OpenSSL installed and use it if found. If not, continue with
    downloading and compiling OpenSSL as before.
    
    Signed-off-by: Patrik Flykt <patrik.flykt@intel.com>

--- bssl-compat/openssl.cmake
+++ bssl-compat/openssl.cmake
@@ -1,11 +1,33 @@
-#  Load OpenSSL as an external project and set up properties to enable build.
-set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-src) # default path by CMake
-set(OPENSSL_BUILD_LOG_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-build-log})
-set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
-set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
-set(OPENSSL_CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/config)
-set(OPENSSL_LIBRARY_SUFFIX so)
-ExternalProject_Add(
+include(CheckIncludeFile)
+
+check_include_file ("openssl/ssl.h" OS_OPENSSL_HEADERS)
+find_library (OS_SSL_LIBRARY ssl)
+find_library (OS_CRYPTO_LIBRARY crypto)
+
+if (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
+    MESSAGE (STATUS "OpenSSL is already installed")
+
+    set(OPENSSL_INCLUDE_DIR "")
+
+    add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
+    set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION "${OS_SSL_LIBRARY}")
+    add_dependencies(OpenSSL::SSL OpenSSL)
+
+    add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
+    set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION "${OS_CRYPTO_LIBRARY}")
+    add_dependencies(OpenSSL::Crypto OpenSSL)
+
+else (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
+    MESSAGE (STATUS "Installing OpenSSL as external project")
+
+    #  Load OpenSSL as an external project and set up properties to enable build.
+    set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-src) # default path by CMake
+    set(OPENSSL_BUILD_LOG_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-build-log})
+    set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
+    set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
+    set(OPENSSL_CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/config)
+    set(OPENSSL_LIBRARY_SUFFIX so)
+    ExternalProject_Add(
         OpenSSL
         SOURCE_DIR ${OPENSSL_SOURCE_DIR}
         URL ${OPENSSL_URL}
@@ -20,14 +42,17 @@ ExternalProject_Add(
         TEST_COMMAND ""
         INSTALL_COMMAND make install
         INSTALL_DIR ${OPENSSL_INSTALL_DIR}
-)
+    )
 
-add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
-set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libssl.${OPENSSL_LIBRARY_SUFFIX})
-set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
-add_dependencies(OpenSSL::SSL OpenSSL)
+    add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
+    set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libssl.${OPENSSL_LIBRARY_SUFFIX})
+    set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
+    add_dependencies(OpenSSL::SSL OpenSSL)
 
-add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
-set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libcrypto.${OPENSSL_LIBRARY_SUFFIX})
-set_property(TARGET OpenSSL::Crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
-add_dependencies(OpenSSL::Crypto OpenSSL)
+    add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
+    set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libcrypto.${OPENSSL_LIBRARY_SUFFIX})
+    set_property(TARGET OpenSSL::Crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
+    add_dependencies(OpenSSL::Crypto OpenSSL)
+
+endif (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
+
