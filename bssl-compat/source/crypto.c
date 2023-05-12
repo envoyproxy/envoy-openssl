73a11d0ec5e8ce426c8d34ab0b4c597c0eb5b296
Ted Poole <tpoole@redhat.com>
2023-05-12 12:10:59 +0100

    OSSM-3364 Fixed FIPS_mode()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/crypto.c
+++ bssl-compat/source/crypto.c
@@ -1,8 +1,7 @@
 #include <openssl/crypto.h>
-#include <ossl/openssl/crypto.h>
 #include <ossl/openssl/evp.h>
 
 
 int FIPS_mode(void) {
-  return ossl_FIPS_mode();
+  return ossl_EVP_default_properties_is_fips_enabled(NULL);
 }
