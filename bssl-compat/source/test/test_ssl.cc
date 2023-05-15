dca2708cb5e02615e0101a8ece423cf648e09fab
Ted Poole <tpoole@redhat.com>
2023-05-15 13:51:24 +0100

    OSSM-3364 Added SSL_get_cipher_by_value()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_ssl.cc
+++ bssl-compat/source/test/test_ssl.cc
@@ -481,3 +481,32 @@ TEST(SSLTest, test_SSL_early_callback_ctx_extension_get) {
 
   server.join();
 }
+
+TEST(SSLTest, test_SSL_get_cipher_by_value) {
+  struct {
+    uint16_t value; // IANA number
+    const char *name; // IETF name
+  }
+  ciphers[] {
+    { 0x1302, "TLS_AES_256_GCM_SHA384" },
+    { 0x1303, "TLS_CHACHA20_POLY1305_SHA256" },
+    { 0x1301, "TLS_AES_128_GCM_SHA256" },
+    { 0xc02c, "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" },
+    { 0xcca9, "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256" },
+    { 0xc02b, "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" },
+    { 0xc00a, "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA" },
+    { 0xc009, "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA" },
+    { 0x009d, "TLS_RSA_WITH_AES_256_GCM_SHA384" },
+    { 0x009c, "TLS_RSA_WITH_AES_128_GCM_SHA256" },
+    { 0x0035, "TLS_RSA_WITH_AES_256_CBC_SHA" },
+    { 0x002f, "TLS_RSA_WITH_AES_128_CBC_SHA" },
+    { 0x008d, "TLS_PSK_WITH_AES_256_CBC_SHA" },
+    { 0x008c, "TLS_PSK_WITH_AES_128_CBC_SHA" }
+  };
+
+  for(auto &c : ciphers) {
+    const SSL_CIPHER *cipher = SSL_get_cipher_by_value(c.value);
+    ASSERT_TRUE(cipher) << "Failed to get cipher " << c.value << " (" << c.name << ")";
+    EXPECT_STREQ(c.name, SSL_CIPHER_standard_name(cipher));
+  }
+}
