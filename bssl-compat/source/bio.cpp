d3afb8ed81ea47203850b62c3ffb21745a3a48a8
Ted Poole <tpoole@redhat.com>
2023-05-25 17:31:53 +0100

    OSSM-2145 Added BIO_up_ref()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/bio.cpp
+++ bssl-compat/source/bio.cpp
@@ -251,14 +251,6 @@ const BIO_METHOD *BIO_s_socket() {
 }
 
 /*
- * OSSL: https://www.openssl.org/docs/man1.1.1/man3/BIO_up_ref.html
- * BSSL: https://github.com/google/boringssl/blob/cacb5526268191ab52e3a8b2d71f686115776646/src/include/openssl/bio.h#L101
- */
-int BIO_up_ref(BIO *bio) {
-  return ossl_BIO_up_ref(bio);
-}
-
-/*
  * OSSL: https://www.openssl.org/docs/man1.1.1/man3/BIO_vfree.html
  * BSSL: https://github.com/google/boringssl/blob/cacb5526268191ab52e3a8b2d71f686115776646/src/include/openssl/bio.h#L98
  */
