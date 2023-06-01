4b5e20ac9a55809d9e190a9c07253df5e8cf854a
Ted Poole <tpoole@redhat.com>
2023-06-01 18:39:23 +0100

    OSSM-3364 Added SSL_get0_alpn_selected()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/ssl.h.sh
+++ bssl-compat/patch/include/openssl/ssl.h.sh
@@ -15,6 +15,7 @@ SUBSTITUTIONS+=('DTLS1_2_VERSION')
 SUBSTITUTIONS+=('SSL_R_[a-zA-Z0-9_]*')
 SUBSTITUTIONS+=('SSL_TLSEXT_ERR_[A-Z_]*')
 SUBSTITUTIONS+=('SSL_SESS_CACHE_[A-Z_]*')
+SUBSTITUTIONS+=('OPENSSL_NPN_[A-Z_]*')
 
 EXPRE='s|^//[ \t]#[ \t]*define[ \t]*[^a-zA-Z0-9_]\('
 EXPOST='\)[^a-zA-Z0-9_].*$|#ifdef ossl_\1\n#define \1 ossl_\1\n#endif|'
