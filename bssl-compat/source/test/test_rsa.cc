31377b1f865621c84468335f1a45f3cb2aa28892
Ted Poole <tpoole@redhat.com>
2023-06-07 18:41:28 +0100

    OSSM-3362 Added RSA_get0_key()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_rsa.cc
+++ bssl-compat/source/test/test_rsa.cc
@@ -10,5 +10,15 @@ TEST(RSATest, test_RSA_set0_factors) {
 
 TEST(RSATest, test_RSA_set0_key) {
   bssl::UniquePtr<RSA> key {RSA_new()};
-  ASSERT_EQ(1, RSA_set0_key(key.get(), BN_new(), BN_new(), BN_new()));
+  BIGNUM *n {BN_new()};
+  BIGNUM *e {BN_new()};
+  BIGNUM *d {BN_new()};
+  ASSERT_EQ(1, RSA_set0_key(key.get(), n, e, d));
+  const BIGNUM *n2 {};
+  const BIGNUM *e2 {};
+  const BIGNUM *d2 {};
+  RSA_get0_key(key.get(), &n2, &e2, &d2);
+  ASSERT_EQ(n2, n);
+  ASSERT_EQ(e2, e);
+  ASSERT_EQ(d2, d);
 }
