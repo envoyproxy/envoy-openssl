3b01cd478b6944487fa292920ad074dddcb5b1aa
Ted Poole <tpoole@redhat.com>
2023-06-08 15:56:25 +0100

    OSSM-3362 Added RSA_decrypt()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/rsa.h.sh
+++ bssl-compat/patch/include/openssl/rsa.h.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sed -i -e 's|^// \(#[ \t]*define[ \t]*\)\(RSA_R_[a-zA-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' "$1"
+SUBSTITUTIONS+=('RSA_R_[a-zA-Z0-9_]*')
+SUBSTITUTIONS+=('RSA_[a-zA-Z0-9_]*_PADDING')
 
+EXPRE='s|^//[ \t]#[ \t]*define[ \t]*[^a-zA-Z0-9_]\('
+EXPOST='\)[^a-zA-Z0-9_].*$|#ifdef ossl_\1\n#define \1 ossl_\1\n#endif|'
+
+for SUBSTITUTION in "${SUBSTITUTIONS[@]}"
+do
+	sed -i -e "${EXPRE}${SUBSTITUTION}${EXPOST}" "$1"
+done
