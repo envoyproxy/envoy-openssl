37acd9e03b18876a680f8b3a16d8532960c51062
Ted Poole <tpoole@redhat.com>
2023-06-13 10:19:15 +0100

    OSSM-3362 Fixed RSA_decrypt()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/RSA_decrypt.cc
+++ bssl-compat/source/RSA_decrypt.cc
@@ -7,26 +7,7 @@
  * https://www.openssl.org/docs/man3.0/man3/RSA_private_decrypt.html
  */
 extern "C" int RSA_decrypt(RSA *rsa, size_t *out_len, uint8_t *out, size_t max_out, const uint8_t *in, size_t in_len, int padding) {
-  size_t min_out = RSA_size(rsa);
-
-  switch(padding) {
-    case RSA_NO_PADDING: {
-      break;
-    }
-    case RSA_PKCS1_PADDING: {
-      min_out -= 11;
-      break;
-    }
-    case RSA_PKCS1_OAEP_PADDING: {
-      min_out -= 42;
-      break;
-    }
-    default: {
-      return 0; // Unexpected padding value
-    }
-  }
-
-  if (max_out < min_out) {
+  if (max_out < RSA_size(rsa)) {
     return 0; // out buffer too small
   }
 
