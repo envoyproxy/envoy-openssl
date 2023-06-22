c025933a396b2fee50173bd2c002064c1fe4aa7f
Ted Poole <tpoole@redhat.com>
2023-06-22 15:55:05 +0100

    OSSM-3365 Added more <openssl/x509.h> functions:
    - X509_get_X509_PUBKEY
    - X509_get0_notAfter
    - X509_get0_notBefore
    - X509_PUBKEY_get
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_x509.cc
+++ bssl-compat/source/test/test_x509.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <openssl/x509.h>
 #include <openssl/pem.h>
+#include <openssl/evp.h>
 
 #include "certs/server_1_cert.pem.h"
 #include "../crypto/test/test_util.h"
@@ -23,4 +24,16 @@ TEST(X509Test, test_X509_NAME_digest) {
   uint8_t expected[] {0x19,0x27,0x3b,0xb5,0x60,0x9c,0xa4,0x45,0x9e,0xa8,0x73,0x0d,0x7f,0x5f,0xb5,0xf1,0xd3,0x5c,0x06,0xad,0x3d,0x2b,0x94,0x98,0x1c,0x65,0xb8,0x76,0x8d,0xee,0x15,0xed};
 
   ASSERT_EQ(Bytes(expected), Bytes(buf, len));
+}
+
+TEST(X509Test, test_X509_get_X509_PUBKEY) {
+  bssl::UniquePtr<BIO> bio {BIO_new_mem_buf(server_1_cert_pem_str, strlen(server_1_cert_pem_str))};
+  ASSERT_TRUE(bio);
+  bssl::UniquePtr<X509> cert {PEM_read_bio_X509(bio.get(), nullptr, nullptr, nullptr)};
+  ASSERT_TRUE(cert);
+  X509_PUBKEY *pubkey {X509_get_X509_PUBKEY(cert.get())};
+  ASSERT_TRUE(pubkey);
+  bssl::UniquePtr<EVP_PKEY> pkey {X509_PUBKEY_get(pubkey)};
+  ASSERT_TRUE(pkey);
+  ASSERT_EQ(EVP_PKEY_RSA, EVP_PKEY_id(pkey.get()));
 }
