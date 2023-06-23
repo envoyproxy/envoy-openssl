727d2c709c3255d40cf263a0710c0c78f01e92ba
Ted Poole <tpoole@redhat.com>
2023-06-23 10:33:06 +0100

    OSSM-3365 Added X509_get_key_usage()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/x509v3.h.sh
+++ bssl-compat/patch/include/openssl/x509v3.h.sh
@@ -2,5 +2,6 @@
 
 sed -i -e 's|^// \(#[ \t]*define[ \t]*\)\(X509V3_R_[a-zA-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' \
        -e 's|^// \(#[ \t]*define[ \t]*\)\(GEN_[A-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' \
-       -e 's|^// \(#[ \t]*define[ \t]*\)\(EXFLAG_[A-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' "$1"
+       -e 's|^// \(#[ \t]*define[ \t]*\)\(EXFLAG_[A-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' \
+       -e 's|^// \(#[ \t]*define[ \t]*\)\(KU_[A-Z0-9_]*\)[^a-zA-Z0-9_].*$|#ifdef ossl_\2\n\1\2 ossl_\2\n#endif|g' "$1"
 
