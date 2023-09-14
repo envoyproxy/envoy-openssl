ab65a37588e28693ea1e7a41138d880f8e62ca36
Ted Poole <tpoole@redhat.com>
2023-09-14 17:45:25 +0100

    OSSM-2029 Added bssl::internal::DeleterImpl<EC_KEY>
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_misc.cc
+++ bssl-compat/source/test/test_misc.cc
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 #include <openssl/asn1.h>
+#include <openssl/ec_key.h>
 
 
 TEST(MiscTest, test_UniquePtr_ASN1_OBJECT) {
   bssl::UniquePtr<ASN1_OBJECT> p;
+}
+
+TEST(MiscTest, test_UniquePtr_EC_KEY) {
+  bssl::UniquePtr<EC_KEY> p;
 }
