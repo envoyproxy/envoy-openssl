ca626d0fa61abd88a457e4432f76b41f5053ec59
Ted Poole <tpoole@redhat.com>
2023-09-19 11:05:26 +0100

    OSSM-2029 Added ECDSA_SIG_get0()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/ecdsa.h.sh
+++ bssl-compat/patch/include/openssl/ecdsa.h.sh
@@ -7,6 +7,7 @@ uncomment.sh "$1" --comment -h \
   --uncomment-func-decl ECDSA_size \
   --uncomment-func-decl ECDSA_SIG_new \
   --uncomment-func-decl ECDSA_SIG_free \
+  --uncomment-func-decl ECDSA_SIG_get0 \
   --uncomment-func-decl ECDSA_do_verify \
   --uncomment-struct ecdsa_sig_st \
   --uncomment-macro-redef 'ECDSA_R_[[:alnum:]_]*' \
