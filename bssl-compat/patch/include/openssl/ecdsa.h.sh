539584ea588bb48830ce99dd874ee446b12e7493
Ted Poole <tpoole@redhat.com>
2023-09-19 10:32:59 +0100

    OSSM-2029 Added ECDSA_do_verify()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/ecdsa.h.sh
+++ bssl-compat/patch/include/openssl/ecdsa.h.sh
@@ -7,6 +7,7 @@ uncomment.sh "$1" --comment -h \
   --uncomment-func-decl ECDSA_size \
   --uncomment-func-decl ECDSA_SIG_new \
   --uncomment-func-decl ECDSA_SIG_free \
+  --uncomment-func-decl ECDSA_do_verify \
   --uncomment-struct ecdsa_sig_st \
   --uncomment-macro-redef 'ECDSA_R_[[:alnum:]_]*' \
   --uncomment-regex 'BORINGSSL_MAKE_DELETER(ECDSA_SIG,'
