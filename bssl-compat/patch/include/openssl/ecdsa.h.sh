29d833fbdc1c9ed2d0237799b0a28bd2c3759d3e
Ted Poole <tpoole@redhat.com>
2023-09-19 08:24:53 +0100

    OSSM-2029 Added bssl::UniquePtr<ECDSA_SIG> support
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/patch/include/openssl/ecdsa.h.sh
+++ bssl-compat/patch/include/openssl/ecdsa.h.sh
@@ -6,5 +6,7 @@ uncomment.sh "$1" --comment -h \
   --uncomment-func-decl ECDSA_sign \
   --uncomment-func-decl ECDSA_size \
   --uncomment-func-decl ECDSA_SIG_new \
+  --uncomment-func-decl ECDSA_SIG_free \
   --uncomment-struct ecdsa_sig_st \
-  --uncomment-macro-redef 'ECDSA_R_[[:alnum:]_]*'
+  --uncomment-macro-redef 'ECDSA_R_[[:alnum:]_]*' \
+  --uncomment-regex 'BORINGSSL_MAKE_DELETER(ECDSA_SIG,'
--- bssl-compat/source/test/test_misc.cc
+++ bssl-compat/source/test/test_misc.cc
@@ -2,6 +2,7 @@
 #include <openssl/asn1.h>
 #include <openssl/ec_key.h>
 #include <openssl/digest.h>
+#include <openssl/ecdsa.h>
 
 
 TEST(MiscTest, test_UniquePtr_ASN1_OBJECT) {
@@ -14,5 +15,9 @@ TEST(MiscTest, test_UniquePtr_EC_KEY) {
 
 TEST(MiscTest, test_UniquePtr_EVP_MD_CTX) {
   bssl::UniquePtr<EVP_MD_CTX> p;
+}
+
+TEST(MiscTest, test_UniquePtr_ECDSA_SIG) {
+  bssl::UniquePtr<ECDSA_SIG> p;
 }
 
