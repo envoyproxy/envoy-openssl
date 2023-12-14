4b204eb6b505c84bf439788741f83ba5ecf78eb1
Ted Poole <tpoole@redhat.com>
2023-12-14 16:11:13 +0000

    OSSM-5001 Added PEM_read_bio_X509_AUX() implementation in bssl-compat
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_pem.cc
+++ bssl-compat/source/test/test_pem.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <openssl/pem.h>
 #include <openssl/bio.h>
+#include <openssl/x509v3.h>
 #include "certs/root_ca_cert.pem.h"
 #include "certs/root_ca_key.pem.h"
 
@@ -81,4 +82,55 @@ TEST(PEMTest, test_PEM_write_bio_X509) {
     for(size_t i = 0; i < strlen(root_ca_cert_pem_str); i++) {
         ASSERT_EQ(m->data[i], root_ca_cert_pem_str[i]);
     }
+}
+
+TEST(PEMTest, test_PEM_read_bio_X509_AUX) {
+    // This certificate has extra trust information (serverAuth) which is only
+    // processed by the "AUX" versions of the PEM_read/write_bio_X509() functions.
+    const char *cert_pem_str {
+        "-----BEGIN TRUSTED CERTIFICATE-----\n"
+        "MIIEBzCCAu+gAwIBAgIUNT7DdWdpRF5GXsgldAUwg/w9ofMwDQYJKoZIhvcNAQEL"
+        "BQAwgYoxCzAJBgNVBAYTAkdCMRYwFAYDVQQIDA1UeW5lIGFuZCBXZWFyMRwwGgYD"
+        "VQQHDBNOZXdjYXN0bGUgdXBvbiBUeW5lMRAwDgYDVQQKDAdSZWQgSGF0MRwwGgYD"
+        "VQQLDBNSZWQgSGF0IEVuZ2luZWVyaW5nMRUwEwYDVQQDDAxUZXN0IFJvb3QgQ0Ew"
+        "HhcNMjMwNDE5MTg0MTQ1WhcNMjUwNDE4MTg0MTQ1WjCBijELMAkGA1UEBhMCR0Ix"
+        "FjAUBgNVBAgMDVR5bmUgYW5kIFdlYXIxHDAaBgNVBAcME05ld2Nhc3RsZSB1cG9u"
+        "IFR5bmUxEDAOBgNVBAoMB1JlZCBIYXQxHDAaBgNVBAsME1JlZCBIYXQgRW5naW5l"
+        "ZXJpbmcxFTATBgNVBAMMDFRlc3QgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQAD"
+        "ggEPADCCAQoCggEBAMxXI2ABGv7MVb/vLKtgR6PJQzLIXsQHnIb80JuWAAbDujwO"
+        "a6YuYypjpbF5sZaxNK2KHZH7IuBRNy8asiz8xjt/BK3+xneAIWYGGYtIR+l2teho"
+        "eRCBdoIGruVzrHuE+FWJqt7f5eiOtbzzAeoKXMLiTwX8CF9NA0ugDjLZRlSzRS3d"
+        "chKdgq542sOQ0vwiDtdTovmZT/5RweGumZ1uvAUP8DynrxKdzffv0c30nEpcCbOI"
+        "SqSRgDUwOwCL2dXshJM3EBV0Ycn+yvvqCvDlWtqILvStw9gTHNdo+fn7g2v+/GZb"
+        "OnOx1Yp9JlzcN5JchKd47QwvX7Llk7WV4rniv3ECAwEAAaNjMGEwDwYDVR0TAQH/"
+        "BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYEFFG0cchLrL8gNr/H1Ykr"
+        "XoKcRfeUMB8GA1UdIwQYMBaAFFG0cchLrL8gNr/H1YkrXoKcRfeUMA0GCSqGSIb3"
+        "DQEBCwUAA4IBAQBonfu+Om04XKWQU6PQlAPoz3vxYec9UYSdL32wYiMEBj8OO/Sv"
+        "7a9aYRpahkX27pT7J0erL9jR+p+gAtmvCWl7SYzfb0LQzUgPPImb5bcEZ/Q80+Uv"
+        "IffCl3ChWEqErg0JJdMSc5qj1WpTU9+Y4YXMddviYsQgVNDea7w1v+hFAp4BlyKP"
+        "OE8h9TQDQErkxI+3kulc2X7OSvGFUDVJpSTPdYd68uSuW4pf7E9l2SbNDaTqNKFI"
+        "EyhYYl6v/HUBYNiPnRUPHdkz+Ypt/9+YxkdNc680q0mGDjU1tWZX0/XVE3pETp52"
+        "tysI3R1cdVCam29BtNXUHfhBxjfxtH+zvhjKMAwwCgYIKwYBBQUHAwE=\n"
+        "-----END TRUSTED CERTIFICATE-----\n"
+    };
+
+    bssl::UniquePtr<BIO> pem1 {BIO_new_mem_buf(cert_pem_str, strlen(cert_pem_str))};
+    ASSERT_TRUE(pem1);
+
+    // Reading the certificate with the "AUX" PEM_read_bio_X509_AUX() function
+    // will read in the additional trust information from the PEM string.
+    bssl::UniquePtr<X509> cert {PEM_read_bio_X509_AUX(pem1.get(), nullptr, nullptr, nullptr)};
+    ASSERT_TRUE(cert) << ERR_error_string(ERR_get_error(), nullptr);
+
+    // Writing the certificate with the "normal" PEM_write_bio_X509() function
+    // will omit the additional trust information from the resulting PEM string
+    bssl::UniquePtr<BIO> pem2 {BIO_new(BIO_s_mem())};
+    ASSERT_TRUE(PEM_write_bio_X509(pem2.get(), cert.get()));
+
+    BUF_MEM *m1 {nullptr};
+    BIO_get_mem_ptr(pem2.get(), &m1);
+    ASSERT_TRUE(m1);
+
+    // Check the PEM_write_bio_X509() output is shorter than the original input
+    ASSERT_TRUE(m1->length < strlen(cert_pem_str));
 }
