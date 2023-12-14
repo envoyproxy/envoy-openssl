62cf7c7c6d65aec479ee07bcfe119219dc64a719
Ted Poole <tpoole@redhat.com>
2023-12-14 13:14:07 +0000

    OSSM-5001 Added PEM_write_bio_X509() implementation in bssl-compat
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/test/test_pem.cc
+++ bssl-compat/source/test/test_pem.cc
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <openssl/pem.h>
 #include <openssl/bio.h>
-//#include <openssl/evp.h>
 #include "certs/root_ca_cert.pem.h"
 #include "certs/root_ca_key.pem.h"
 
@@ -59,4 +58,27 @@ TEST(PEMTest, testPEM_read) {
     }
 
     BIO_free(pem);
+}
+
+TEST(PEMTest, test_PEM_write_bio_X509) {
+    bssl::UniquePtr<BIO> pem1 {BIO_new_mem_buf(root_ca_cert_pem_str, strlen(root_ca_cert_pem_str))};
+    ASSERT_TRUE(pem1);
+
+    bssl::UniquePtr<X509> cert {PEM_read_bio_X509(pem1.get(), NULL, 0, NULL)};
+    ASSERT_TRUE(cert) << ERR_error_string(ERR_get_error(), nullptr);
+
+    bssl::UniquePtr<BIO> pem2 {BIO_new(BIO_s_mem())};
+    ASSERT_TRUE(PEM_write_bio_X509(pem2.get(), cert.get()));
+
+    BUF_MEM *m {nullptr};
+    BIO_get_mem_ptr(pem2.get(), &m);
+    ASSERT_TRUE(m);
+
+    // Check the PEM_write_bio_X509() output is the
+    // same as the original root_ca_cert_pem_str input
+
+    ASSERT_EQ(m->length, strlen(root_ca_cert_pem_str));
+    for(size_t i = 0; i < strlen(root_ca_cert_pem_str); i++) {
+        ASSERT_EQ(m->data[i], root_ca_cert_pem_str[i]);
+    }
 }
