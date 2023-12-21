f2697d1d9bb18e404bb7ef971dd8d588a4bf7412
Ted Poole <tpoole@redhat.com>
2023-12-21 09:57:41 +0000

    OSSM-5001 Tidy up bssl-compat install process
    There were some unnecessary files getting installed as part of the
    bssl-compat installation process. In particular, the googletest headers
    were being installed, which was upsetting the envoy build.
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/cmake/openssl.cmake
+++ bssl-compat/cmake/openssl.cmake
@@ -1,61 +1,21 @@
-include(CheckIncludeFile)
-include(ExternalProject)
+find_package(OpenSSL 3.0 COMPONENTS Crypto SSL)
 
-check_include_file ("openssl/types.h" OS_OPENSSL_HEADERS)
-find_library (OS_SSL_LIBRARY ssl)
-find_library (OS_CRYPTO_LIBRARY crypto)
-
-if (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
-    MESSAGE (STATUS "OpenSSL is already installed")
-
-    set(OPENSSL_INCLUDE_DIR "")
-
-    add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION "${OS_SSL_LIBRARY}")
-    add_dependencies(OpenSSL::SSL OpenSSL)
-
-    add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION "${OS_CRYPTO_LIBRARY}")
-    add_dependencies(OpenSSL::Crypto OpenSSL)
-
-    find_path(SRC_OPENSSL_HEADERS1 openssl/ssl.h)
-    find_path(SRC_OPENSSL_HEADERS2 openssl/opensslconf.h)
-
-else (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
-    MESSAGE (STATUS "Installing OpenSSL as external project")
-
-    #  Load OpenSSL as an external project and set up properties to enable build.
-    set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-src) # default path by CMake
-    set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
+if(OpenSSL_FOUND)
+    get_filename_component(OPENSSL_LIBRARY_DIR ${OPENSSL_CRYPTO_LIBRARY} DIRECTORY)
+    message(STATUS "Found OpenSSL ${OPENSSL_VERSION} (${OPENSSL_LIBRARY_DIR})")
+else()
+    message(STATUS "Building OpenSSL (${OPENSSL_URL})")
+    set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl/source)
+    set(OPENSSL_CONFIG_CMD ${OPENSSL_SOURCE_DIR}/config)
+    set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl/install)
     set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
-    set(OPENSSL_CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/config)
-    set(OPENSSL_LIBRARY_SUFFIX so)
-    ExternalProject_Add(
-        OpenSSL
-        SOURCE_DIR ${OPENSSL_SOURCE_DIR}
+    set(OPENSSL_LIBRARY_DIR ${OPENSSL_INSTALL_DIR}/lib)
+    ExternalProject_Add(OpenSSL
         URL ${OPENSSL_URL}
         URL_HASH SHA256=${OPENSSL_URL_HASH}
-        USES_TERMINAL_DOWNLOAD TRUE
-        CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/config
-        --prefix=${OPENSSL_INSTALL_DIR}
-        --openssldir=${OPENSSL_INSTALL_DIR}
-        --libdir=lib
+        SOURCE_DIR ${OPENSSL_SOURCE_DIR}
+        CONFIGURE_COMMAND ${OPENSSL_CONFIG_CMD} --prefix=${OPENSSL_INSTALL_DIR} --libdir=lib
         TEST_COMMAND ""
         INSTALL_COMMAND make install_sw
-        INSTALL_DIR ${OPENSSL_INSTALL_DIR}
     )
-    file(MAKE_DIRECTORY ${OPENSSL_INCLUDE_DIR})
-    add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libssl.${OPENSSL_LIBRARY_SUFFIX})
-    set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
-    add_dependencies(OpenSSL::SSL OpenSSL)
-
-    add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libcrypto.${OPENSSL_LIBRARY_SUFFIX})
-    set_property(TARGET OpenSSL::Crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
-    add_dependencies(OpenSSL::Crypto OpenSSL)
-
-	set(SRC_OPENSSL_HEADERS1 ${OPENSSL_INCLUDE_DIR})
-	set(SRC_OPENSSL_HEADERS2 ${OPENSSL_INCLUDE_DIR})
-
-endif (OS_OPENSSL_HEADERS AND  OS_SSL_LIBRARY AND OS_CRYPTO_LIBRARY)
+endif()
