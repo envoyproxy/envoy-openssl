93feebcf8058cb44f6b38fc3491ebe8e9c616b86
Ted Poole <tpoole@redhat.com>
2024-03-28 09:57:17 +0000

    Code changes to compile on bssl-compat/openssl
    Signed-off-by: Ted Poole <tpoole@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- test/integration/sds_dynamic_integration_test.cc
+++ test/integration/sds_dynamic_integration_test.cc
@@ -1152,7 +1152,7 @@ INSTANTIATE_TEST_SUITE_P(IpVersionsClientType, SdsDynamicDownstreamPrivateKeyInt
                          testing::ValuesIn(getSdsTestsParams(true)), sdsTestParamsToString);
 
 // Validate that a basic SDS updates work with a private key provider.
-TEST_P(SdsDynamicDownstreamPrivateKeyIntegrationTest, BasicPrivateKeyProvider) {
+TEST_P(SdsDynamicDownstreamPrivateKeyIntegrationTest, DISABLED_BasicPrivateKeyProvider) {
   v3_resource_api_ = true;
 
   TestEnvironment::exec(
@@ -1212,7 +1212,7 @@ INSTANTIATE_TEST_SUITE_P(IpVersionsClientType, SdsCdsPrivateKeyIntegrationTest,
                          testing::ValuesIn(getSdsTestsParams(true)), sdsTestParamsToString);
 
 // Test private key providers in SDS+CDS setup.
-TEST_P(SdsCdsPrivateKeyIntegrationTest, BasicSdsCdsPrivateKeyProvider) {
+TEST_P(SdsCdsPrivateKeyIntegrationTest, DISABLED_BasicSdsCdsPrivateKeyProvider) {
   v3_resource_api_ = true;
 
   TestEnvironment::exec(
