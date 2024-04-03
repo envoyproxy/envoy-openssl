85eb1cc1af45c134f2ae8f6af94c0f270818f6c2
Ted Poole <tpoole@redhat.com>
2024-04-03 18:51:42 +0100

    Replaced BoringSSL submodule with a copy (ca1690e221677cea3fb946f324eb89d846ec53f2)
    BoringSSL Commit ca1690e221677cea3fb946f324eb89d846ec53f2
    Now in the bssl-compat/third_party/boringssl/ directory
    According to https://boringssl.googlesource.com/boringssl/+/HEAD/INCORPORATING.md
    
    Disabled the configure/build for BoringSSL because (1) it can't be done on all
    platforms, and (2) we no longer need to configure/build BoringSSL to obtain it's
    crypto_test_data.cc file because it is now checked in.
    
    Removed the pre installation of go into the builder image. This was only being done
    as a work around to support the BoringSSL configure/build, but that requirement has
    now gone.
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- openssl/do_ci.sh
+++ openssl/do_ci.sh
@@ -10,7 +10,6 @@ cd "$(dirname "$0")/.."
 # environment appropriately for building and testing envoy-openssl.
 echo "test --test_env=LD_LIBRARY_PATH=${ENVOY_OPENSSL_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}" > openssl.bazelrc
 echo "test --test_env=OPENSSL_CONF=$(pwd)/bssl-compat/source/test/openssl.cnf" >> openssl.bazelrc
-export PATH="${ENVOY_OPENSSL_PATH}:${PATH}"
 
 # Hand off to the upstream do_ci.sh script
 exec ./ci/do_ci.sh "$@"
