f1360c5142cb4550a6e6151db2f0d34b82162277
Ted Poole <tpoole@redhat.com>
2024-05-13 15:25:50 +0100

    Initial implementation of SSL_CTX_set_custom_verify()
    * Only supports synchronous (pass or fail) verification, which is enough to
    accommodate the default certificate validator.
    
    * Also fixed/extended the implementation of SSL_get_peer_full_cert_chain()
    so that (1) it's return value now has the correct ownership semantics, and
    (2) it works in the context of a SSL_CTX_set_custom_verify() callback.
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/SSL_set_enforce_rsa_key_usage.cc
+++ bssl-compat/source/SSL_set_enforce_rsa_key_usage.cc
@@ -4,6 +4,6 @@
 
 
 extern "C" void SSL_set_enforce_rsa_key_usage(SSL *ssl, int enabled) {
-  bssl_compat_fatal("SSL_set_enforce_rsa_key_usage() is not implemented");
+  bssl_compat_warn("SSL_set_enforce_rsa_key_usage() is not implemented");
 }
 
--- bssl-compat/source/SSL_was_key_usage_invalid.cc
+++ bssl-compat/source/SSL_was_key_usage_invalid.cc
@@ -4,6 +4,6 @@
 
 
 extern "C" int SSL_was_key_usage_invalid(const SSL *ssl) {
-  bssl_compat_fatal("SSL_was_key_usage_invalid() is not implemented");
+  bssl_compat_warn("SSL_was_key_usage_invalid() is not implemented");
   return 0;
 }
