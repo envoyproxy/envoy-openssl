ea6c2c35f96835f320dfb5b06939e58dbabaa9a1
Daniel Grimm <dgrimm@redhat.com>
2024-05-16 12:45:08 +0200

    Add some missing symbols
    Signed-off-by: Daniel Grimm <dgrimm@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- bssl-compat/patch/include/openssl/ec_key.h.sh
+++ bssl-compat/patch/include/openssl/ec_key.h.sh
@@ -7,5 +7,8 @@ uncomment.sh "$1" --comment -h \
   --uncomment-func-decl EC_KEY_get0_group \
   --uncomment-func-decl EC_KEY_parse_private_key \
   --uncomment-func-decl EC_KEY_new_by_curve_name \
+  --uncomment-func-decl EC_KEY_set_public_key \
   --uncomment-func-decl EC_KEY_set_public_key_affine_coordinates \
+  --uncomment-func-decl EC_KEY_set_private_key \
+  --uncomment-func-decl EC_KEY_get0_private_key \
   --uncomment-regex 'BORINGSSL_MAKE_DELETER(EC_KEY'
