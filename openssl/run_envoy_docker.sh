934f3de1ff8fe7e9f34db386cf39f6c3478eb8da
Ted Poole <tpoole@redhat.com>
2024-05-16 13:02:15 +0100

    Simplified the use of OpenSSL 3.0.x in the build container
    Eliminated the need for the openssl/do_ci.sh script, so the
    upstream ci/do_ci.sh script should now be used directly instead.
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- openssl/run_envoy_docker.sh
+++ openssl/run_envoy_docker.sh
@@ -11,12 +11,6 @@ SCRATCH_DIR="$(mktemp -d)"
 trap 'rm -rf -- "$SCRATCH_DIR"' EXIT
 
 # Create our extended builder image, based on upstream's builder image.
-# Note that the upstream image runs a new bash *login* shell (as the envoybuild
-# user) so any modifications to PATH or LD_LIBRARY_PATH in this Dockerfile will
-# not be  present in that user's environment in the container. Therefore, we
-# just set environment variables with different names, which subsequent
-# scripts such as openssl/do_ci.sh pick up and use to modify the actual PATH
-# and LD_LIBRARY_PATH variables appropriately inside the container.
 docker build --pull --iidfile "${SCRATCH_DIR}/iid" -f - "${SCRATCH_DIR}" << EOF
     FROM $(./ci/run_envoy_docker.sh 'echo $ENVOY_BUILD_IMAGE')
 
@@ -26,12 +20,14 @@ docker build --pull --iidfile "${SCRATCH_DIR}/iid" -f - "${SCRATCH_DIR}" << EOF
     RUN apt update
 
     # Install OpenSSL 3.0.x
+    ENV OPENSSL_VERSION=3.0.8
+    ENV OPENSSL_ROOTDIR=/usr/local/openssl-\$OPENSSL_VERSION
     RUN apt install -y build-essential checkinstall zlib1g-dev
-    RUN wget -qO- https://github.com/openssl/openssl/releases/download/openssl-3.0.8/openssl-3.0.8.tar.gz | tar xz -C /
-    RUN cd /openssl-3.0.8 && ./config -d --prefix=/usr/local/openssl-3.0.8 --openssldir=/usr/local/openssl-3.0.8
-    RUN make -C /openssl-3.0.8 -j && make -C /openssl-3.0.8 install_sw
-    ENV OPENSSL_ROOT_DIR=/usr/local/openssl-3.0.8
-    ENV ENVOY_OPENSSL_LD_LIBRARY_PATH=\$OPENSSL_ROOT_DIR/lib64
+    RUN wget -qO- https://github.com/openssl/openssl/releases/download/openssl-\$OPENSSL_VERSION/openssl-\$OPENSSL_VERSION.tar.gz | tar xz -C /
+    RUN cd /openssl-\$OPENSSL_VERSION && ./config -d --prefix=\$OPENSSL_ROOTDIR --openssldir=\$OPENSSL_ROOTDIR
+    RUN make -C /openssl-\$OPENSSL_VERSION -j && make -C /openssl-\$OPENSSL_VERSION install_sw
+    RUN echo "\$OPENSSL_ROOTDIR/lib64" > /etc/ld.so.conf.d/openssl-\$OPENSSL_VERSION.conf
+    RUN ldconfig
 EOF
 
 
