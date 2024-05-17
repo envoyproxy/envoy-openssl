ee0927d852f08416a242e2ec67ceb0bf186fb67e
Ted Poole <tpoole@redhat.com>
2024-05-17 14:02:27 +0100

    Fixed some test fingerprints & byte counts to match OpenSSL instead of BoringSSL
    Some tests check for things like JA3 fingerprints and/or received byte
    counts, which vary between BoringSSL and OpenSSL due to slightly
    different client hello contents etc.
    
    Signed-off-by: Ted Poole <tpoole@redhat.com>
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- test/extensions/filters/listener/tls_inspector/tls_inspector_integration_test.cc
+++ test/extensions/filters/listener/tls_inspector/tls_inspector_integration_test.cc
@@ -199,7 +199,7 @@ TEST_P(TlsInspectorIntegrationTest, JA3FingerprintIsSet) {
   client_->close(Network::ConnectionCloseType::NoFlush);
 
   EXPECT_THAT(waitForAccessLog(listener_access_log_name_),
-              testing::Eq("71d1f47d1125ac53c3c6a4863c087cfe"));
+              testing::Eq("54619c7296adab310ed514d06812d95f"));
 
   test_server_->waitUntilHistogramHasSamples("tls_inspector.bytes_processed");
   auto bytes_processed_histogram = test_server_->histogram("tls_inspector.bytes_processed");
@@ -208,7 +208,7 @@ TEST_P(TlsInspectorIntegrationTest, JA3FingerprintIsSet) {
       1);
   EXPECT_EQ(static_cast<int>(TestUtility::readSampleSum(test_server_->server().dispatcher(),
                                                         *bytes_processed_histogram)),
-            115);
+            145);
 }
 
 // The `JA4` fingerprint is correct in the access log.
@@ -281,7 +281,7 @@ TEST_P(TlsInspectorIntegrationTest, RequestedBufferSizeCanGrow) {
       1);
   EXPECT_EQ(static_cast<int>(TestUtility::readSampleSum(test_server_->server().dispatcher(),
                                                         *bytes_processed_histogram)),
-            515);
+            405);
 }
 
 // This test verifies that `JA4` fingerprinting works with a malformed ClientHello that
