172e2164a47afb4db39d0e5554b0f8355df7b689
Dario Cillerai <dcillera@redhat.com>
2024-08-07 09:47:27 +0200

    Add ECDSA_verify function to compat. layer
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- bssl-compat/patch/include/openssl/ecdsa.h.sh
+++ bssl-compat/patch/include/openssl/ecdsa.h.sh
@@ -10,6 +10,7 @@ uncomment.sh "$1" --comment -h \
   --uncomment-func-decl ECDSA_SIG_get0 \
   --uncomment-func-decl ECDSA_SIG_set0 \
   --uncomment-func-decl ECDSA_do_verify \
+  --uncomment-func-decl ECDSA_verify \
   --uncomment-struct ecdsa_sig_st \
   --uncomment-macro-redef 'ECDSA_R_[[:alnum:]_]*' \
   --uncomment-regex 'BORINGSSL_MAKE_DELETER(ECDSA_SIG,'
