e99f1c222892d34646bc308676bfbe4d1a817b48
Dario Cillerai <dcillera@redhat.com>
2024-08-22 17:34:16 +0200

    Prevent failure of RevokedIntermediateCertificate
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -114,6 +114,8 @@ RUNTIME_GUARD(envoy_reloadable_features_xds_prevent_resource_copy);
 RUNTIME_GUARD(envoy_restart_features_fix_dispatcher_approximate_now);
 RUNTIME_GUARD(envoy_restart_features_skip_backing_cluster_check_for_sds);
 RUNTIME_GUARD(envoy_restart_features_use_eds_cache_for_ads);
+RUNTIME_GUARD(envoy_restart_features_use_fast_protobuf_hash);
+RUNTIME_GUARD(envoy_reloadable_features_enable_intermediate_ca);
 
 // Begin false flags. Most of them should come with a TODO to flip true.
 
