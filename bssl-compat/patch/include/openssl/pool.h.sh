316dc201da5f2740607117c54388765afcdc06c0
Dario Cillerai <dcillera@redhat.com>
2025-05-30 22:17:15 +0200

    Fix xfcc_integration_test by correcting SSL_get0_peer_certificate in bssl-compat
    Signed-off-by: Dario Cillerai <dcillera@redhat.com>

--- bssl-compat/patch/include/openssl/pool.h.sh
+++ bssl-compat/patch/include/openssl/pool.h.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 uncomment.sh "$1" --comment -h \
   --uncomment-func-decl 'CRYPTO_BUFFER_new' \
   --uncomment-func-decl 'CRYPTO_BUFFER_free' \
-  --uncomment-regex 'BORINGSSL_MAKE_DELETER(CRYPTO_BUFFER,'
+  --uncomment-regex 'BORINGSSL_MAKE_DELETER(CRYPTO_BUFFER,' \
+  --uncomment-regex 'DEFINE_STACK_OF(CRYPTO_BUFFER)'
