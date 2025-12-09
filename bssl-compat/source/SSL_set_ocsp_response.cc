6c9a4fd0a6fdf6d6f1ee4b5de0ab47606aa81ea5
Ted Poole <tpoole@redhat.com>
2025-12-09 12:22:25 +0000

    Fix leak in bssl-compat SSL_set_ocsp_response()
    Signed-off-by: Ted Poole <tpoole@redhat.com>

--- bssl-compat/source/SSL_set_ocsp_response.cc
+++ bssl-compat/source/SSL_set_ocsp_response.cc
@@ -24,7 +24,14 @@
 typedef std::pair<void*,size_t> OcspResponse;
 
 static int index() {
-  static int index {ossl.ossl_SSL_get_ex_new_index(0, nullptr, nullptr, nullptr, nullptr)};
+  static int index {ossl.ossl_SSL_get_ex_new_index(0, nullptr, nullptr, nullptr,
+    +[](void *, void *ptr, CRYPTO_EX_DATA *, int, long, void*) {
+      if(ptr) {
+        OcspResponse *resp {reinterpret_cast<OcspResponse*>(ptr)};
+        ossl.ossl_OPENSSL_free(resp->first);
+        delete resp;
+      }
+    })};
   return index;
 }
 
@@ -70,6 +77,15 @@ extern "C" int SSL_set_ocsp_response(SSL *ssl, const uint8_t *response, size_t r
       // Install our callback to call the real SSL_set_ex_data() function later
       if (ossl_SSL_CTX_set_tlsext_status_cb(ctx, ssl_apply_deferred_ocsp_response_cb) == 0) {
         return 0;
+      }
+
+      // If we have been called previously, from within the same select
+      // certificate callback invocation, there will be an OcspReponse object
+      // squirreled away already. If so, delete it first, so we don't just
+      // overwrite it and create a leak.
+      if(OcspResponse *prev = reinterpret_cast<OcspResponse*>(ossl.ossl_SSL_get_ex_data(ssl, index()))) {
+        ossl.ossl_OPENSSL_free(prev->first);
+        delete prev;
       }
 
       // Store the OCSP response bytes for the callback to pick up later
